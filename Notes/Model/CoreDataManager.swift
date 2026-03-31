5adf1a170bf14edd585fbc373a147905cf2d5ed8
Vivek Dandotiya <vivekdandotiya772@gmail.com>
2026-03-31 18:13:50 +0530

    feat: implement CoreDataManager for local persistence

---
+++ Notes/Model/CoreDataManager.swift
@@ -0,0 +1,30 @@
+//
+//  CoreDataManager.swift
+//  QuickNotes ✨
+//
+//  Created on 2026.
+//
+
+import CoreData
+
+class CoreDataManager {
+    
+    let container: NSPersistentContainer
+
+    init() {
+        container = NSPersistentContainer(name: "NotesContainer")
+    }
+
+    func loadCoreData(completion: @escaping (Bool) -> Void) {
+        container.loadPersistentStores { description, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("Core Data loading error: \(error.localizedDescription)")
+                    completion(false)
+                } else {
+                    completion(true)
+                }
+            }
+        }
+    }
+}
