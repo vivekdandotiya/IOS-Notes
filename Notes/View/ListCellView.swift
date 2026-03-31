be011b81b6c86a09c436457cd9e39245a9b1223c
Vivek Dandotiya <vivekdandotiya772@gmail.com>
2026-03-31 18:14:05 +0530

    feat: implement ListCellView.swift

---
+++ Notes/View/ListCellView.swift
@@ -0,0 +1,29 @@
+//
+//  ListCellView.swift
+//  QuickNotes ✨
+//
+//  Created on 2026.
+//
+
+import SwiftUI
+
+struct ListCellView: View {
+    var note: NoteEntity
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text(note.title ?? "New Note")
+                .lineLimit(1)
+                .font(.headline)
+                .foregroundColor(.primary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text(note.content ?? "No context available")
+                .lineLimit(2)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(.vertical, 8)
+    }
+}
