a615585d275e3f2b3d3b0b3c9ee641a1b43cfc0c
Vivek Dandotiya <vivekdandotiya772@gmail.com>
2026-03-31 18:14:03 +0530

    feat: implement ContentView.swift

---
+++ Notes/View/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  QuickNotes ✨
+//
+//  Created on 2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var notesViewModel: NotesViewModel
+
+    var body: some View {
+        
+        Group {
+            if notesViewModel.isDataLoaded {
+                NotesView()
+            } else {
+                ProgressView("Loading...")
+            }
+        }
+    }
+}
